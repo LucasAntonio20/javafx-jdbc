3d12e702f6e387fb07f20d9e916c273357cc1f73
Lucas Antonio <lucasantonio2063@gmail.com>
2023-05-09 19:35:26 -0300

    Main view design

--- src/main/java/com/lucasantonio/projetobd/Main.java
+++ src/main/java/com/lucasantonio/projetobd/Main.java
@@ -2,8 +2,8 @@ package com.lucasantonio.projetobd;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,9 +13,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent parent = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+            ScrollPane scrollPane = FXMLLoader.load(getClass().getResource("MainView.fxml"));
             primaryStage.setTitle("Sample JavaFX application");
-            Scene mainScene = new Scene(parent);
+            Scene mainScene = new Scene(scrollPane);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
             primaryStage.setScene(mainScene);
             primaryStage.show();
         } catch (IOException e) {
