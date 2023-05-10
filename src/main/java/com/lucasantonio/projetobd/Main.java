0efa82c7ab5ddb57321e52b21bdc5fafcb754252
Lucas Antonio <lucasantonio2063@gmail.com>
2023-05-10 11:11:11 -0300

    About view

--- src/main/java/com/lucasantonio/projetobd/Main.java
+++ src/main/java/com/lucasantonio/projetobd/Main.java
@@ -10,12 +10,14 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) {
         try {
             ScrollPane scrollPane = FXMLLoader.load(getClass().getResource("MainView.fxml"));
             primaryStage.setTitle("Sample JavaFX application");
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
             primaryStage.setScene(mainScene);
@@ -23,6 +25,10 @@ public class Main extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
